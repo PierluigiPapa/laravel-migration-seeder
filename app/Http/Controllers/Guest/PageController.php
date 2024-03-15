ba098942faa4d6a41ade4eeb520e60d1fd43ee19
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-15 14:55:25 +0100

    feat: add table without print

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace App\Http\Controllers\Guest;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Models\Train;
 
 
@@ -13,6 +11,6 @@ class PageController extends Controller
     public function index()
     {
         $trains = Train::all();
-        return view('welcome');
+        return view('pages.home', compact('trains'));
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('page-title') | LARAVEL-MIGRATION-SEEDER</title>
 
     @vite('resources/js/app.js')
 
 </head>
 <body>
 <h1 class="text-center">HOLA! AMIGO!</h1>
+
+@include('partials.header')
+
+@yield('content')
+
+
+@include('partials.footer')
+
 </body>
 </html>
